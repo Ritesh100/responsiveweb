9078a1ec1c0914f2d3a727ec6ca2e55f2b6470b0
ritesh <reteshthapakaji@gmail.com>
2020-10-08 20:33:56 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var data = {
+    "main": "http://vimeo.com/70161501",
+    "videos": [{
+            "url": "https://vimeo.com/71495477"
+        },
+        {
+            "url": "https://vimeo.com/70775770"
+        },
+        {
+            "url": "https://vimeo.com/71238762"
+        },
+        {
+            "url": "https://vimeo.com/70388552"
+        },
+        {
+            "url": "https://vimeo.com/67890000"
+        },
+        {
+            "url": "https://vimeo.com/70777838"
+        },
+        {
+            "url": "https://vimeo.com/38931673"
+        },
+        {
+            "url": "https://vimeo.com/70286906"
+        }
+    ]
+}
+
+Vimeo.init({
+    container: $('.gallery') //DOM element to append the gallery
+});
